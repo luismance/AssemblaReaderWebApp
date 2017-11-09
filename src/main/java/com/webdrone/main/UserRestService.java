3519d1d7a9c7afad7e47d4142b5452334398a691
Luis Mance <luis.mance@manaty.net>
2017-11-10 01:28:33 +0800

    Updated user sync

--- src/main/java/com/webdrone/main/UserRestService.java
+++ src/main/java/com/webdrone/main/UserRestService.java
@@ -18,11 +18,14 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import org.apache.commons.codec.binary.Base64;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.webdrone.assembla.dto.UserAssemblaDto;
 import com.webdrone.dto.RefreshTokenObject;
+import com.webdrone.dto.RequestTokenObject;
 import com.webdrone.dto.UserDto;
 import com.webdrone.dto.UserListDto;
 import com.webdrone.model.User;
@@ -34,20 +37,20 @@ import com.webdrone.util.RESTServiceUtil;
 @Consumes(MediaType.APPLICATION_XML)
 public class UserRestService {
 
-	private static String REQUEST_ACCESS_TOKEN_URL = "https://augdmUP68r57ldacwqjQYw:02535496a26c8df7a1b7dfd92e3c7f80@api.assembla.com/token?grant_type=refresh_token&refresh_token=";
+	private static String REQUEST_REFRESH_TOKEN_URL = "https://api.assembla.com/token?grant_type=authorization_code&code=";
 
-	private static String REFRESH_TOKEN = "26fd9fa890f2130659a6948e10064f60";
+	private static String REQUEST_ACCESS_TOKEN_URL = "https://baX24QXs4r56RcacwqjQXA:040301aea16521d342ed8de1b9d12c9d@api.assembla.com/token?grant_type=refresh_token&refresh_token=";
+
 
 	@Inject
 	private UserService userService;
 
-	@SuppressWarnings("unused")
 	@POST
 	@Path("/create")
 	public Response createUser(@HeaderParam("Authorization") String authorization) {
 
 		try {
-			String response = RESTServiceUtil.sendPOST(REQUEST_ACCESS_TOKEN_URL + REFRESH_TOKEN, true, authorization);
+			String response = RESTServiceUtil.sendPOST(REQUEST_ACCESS_TOKEN_URL + "", true, authorization);
 
 			ObjectMapper objMap = new ObjectMapper();
 			RefreshTokenObject rto = objMap.readValue(response, RefreshTokenObject.class);
@@ -63,7 +66,7 @@ public class UserRestService {
 			UserAssemblaDto userAssemblaDto = (UserAssemblaDto) unmarshaller.unmarshal(new StringReader(userDetails));
 
 			User user = new User(userAssemblaDto.getLogin(), "password", userAssemblaDto.getId(), rto.getAccess_token(),
-					REFRESH_TOKEN, userAssemblaDto.getName(), userAssemblaDto.getEmail(), userAssemblaDto.getPhone());
+					"", userAssemblaDto.getName(), userAssemblaDto.getEmail(), userAssemblaDto.getPhone());
 
 			UserDto userDto = new UserDto(user);
 			userService.create(user);
@@ -82,6 +85,62 @@ public class UserRestService {
 			e.printStackTrace();
 		}
 		return Response.status(500).build();
+	}
+
+	@POST
+	@Path("/registerToken")
+	public Response registerToken(String requestBody) {
+
+		try {
+			System.out.println("Req Body : " + requestBody);
+
+			String authorizationToken = "Basic " + new String(
+					Base64.encodeBase64("baX24QXs4r56RcacwqjQXA:040301aea16521d342ed8de1b9d12c9d".getBytes()));
+
+			String response = RESTServiceUtil.sendPOST(REQUEST_REFRESH_TOKEN_URL + requestBody, true,
+					authorizationToken);
+
+			ObjectMapper objMap = new ObjectMapper();
+
+			RequestTokenObject rto = objMap.readValue(response, RequestTokenObject.class);
+
+			String userDetailsEndpoint = "https://api.assembla.com/v1/user.xml";
+
+			String userDetails = RESTServiceUtil.sendGET(userDetailsEndpoint, true, "Bearer " + rto.getAccess_token());
+
+			System.out.println("User Details : " + userDetails);
+			JAXBContext jxb = JAXBContext.newInstance(UserAssemblaDto.class);
+			Unmarshaller unmarshaller = jxb.createUnmarshaller();
+
+			UserAssemblaDto userAssemblaDto = (UserAssemblaDto) unmarshaller.unmarshal(new StringReader(userDetails));
+
+			User user = new User(userAssemblaDto.getLogin(), "password", userAssemblaDto.getId(), rto.getAccess_token(),
+					rto.getRefresh_token(), userAssemblaDto.getName(), userAssemblaDto.getEmail(),
+					userAssemblaDto.getPhone());
+
+			UserDto userDto = new UserDto(user);
+
+			userService.create(user);
+
+			System.out.println("Response " + response);
+
+			return Response.status(200).entity(userDto).build();
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return Response.status(500).build();
+
 	}
 
 	@GET
